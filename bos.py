0d177bdcb08d8e5061091f87f8867b19c8a47715
j4nties <110736778+j4nties@users.noreply.github.com>
2022-12-11 21:26:24 +0300

    Delete bos.py

--- bos.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-P, N, M = map(int,input().split())
-A = np.array([input().split() for x in range(P)],int)
-B = np.array([input().split() for x in range(N)],int)
-print(np.concatenate((A, B), axis = 0))
-
-
